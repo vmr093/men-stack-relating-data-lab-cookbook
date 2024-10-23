fdd075030ebc93e78e4349d09119e2cf883e7ccb
vmr093 <ranjha.viola@gmail.com>
2024-10-22 19:44:39 -0500

    works

--- controllers/users.js
+++ controllers/users.js
@@ -6,6 +6,14 @@ const User = require('../models/user.js')
 
 
 
+// router.get('/', async (req, res) => {
+//     const allUsers = await User.find()
+//     console.log(allUsers)
+//     res.render('users/index.ejs', {
+//         allUsers
+//     })
+// })
+
 
 router.get('/', async (req, res) => {
     try {
